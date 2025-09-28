de45e433c7adfeba79deaa0b3ce5aa529d527fea
wiron <111750374+Rwiron@users.noreply.github.com>
2025-09-28 10:17:16 +0200

    serveless

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const serverless = require('serverless-http');
+
+module.exports = async function handler(req, res) {
+  const { createApp } = require('../dist/app');
+  const app = await createApp();
+  const handle = serverless(app);
+  return handle(req, res);
+};
+
+
